463f9258ba2f99b1634e14ae2bf3dc94e2701256
anchit mandal <21f1000692@ds.study.iitm.ac.in>
2024-03-12 13:24:58 +0530

    modified:   app.py 	modified:   instance/mydb.sqlite3 	modified:   models.py 	new file:   templates/form_test.html 	new file:   templates/login.html 	modified:   templates/test.html

--- app.py
+++ app.py
@@ -1,26 +1,97 @@
-from flask import Flask, request, render_template
-# from models import db, User, list, task
-# from flask import *
+from flask import Flask, request, render_template, redirect, url_for, session
 from models import *
+from flask_security import Security, SQLAlchemyUserDatastore
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./mydb.sqlite3'
+# Generate a nice key using secrets.token_urlsafe()
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", 'pf9Wkove4IKEAXvy-cQkeDPhv9Cb3Ag-wyJILbq_dFw')
+# Bcrypt is set as default SECURITY_PASSWORD_HASH, which requires a salt
+# Generate a good salt using: secrets.SystemRandom().getrandbits(128)
+app.config['SECURITY_PASSWORD_SALT'] = os.environ.get("SECURITY_PASSWORD_SALT", '146585145368132386173505678016728509634')
+app.config['SECURITY_REGISTERABLE'] = True
+# Don't worry if email has findable domain
+app.config["SECURITY_EMAIL_VALIDATOR_ARGS"] = {"check_deliverability": False}
+app.config['SECURITY_SEND_REGISTER_EMAIL'] = False
+app.config['SECURITY_POST_REGISTER_VIEW'] = '/test'
+app.config['SECURITY_POST_LOGIN_VIEW'] = '/get_data_abcd'
+app.config['SECURITY_POST_LOGOUT_VIEW'] = '/test'
+
 
 
 db.init_app(app)
 app.app_context().push()
 
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
+
+
+@app.route('/')
+def index():
+    return 'you are on the root endpoint'
+
 @app.route('/test', methods=['GET', 'POST', 'PUT'])
 def hello_world():
     if request.method == 'POST':
+        name = request.form.get('l_name')
+        key = request.form.get('l_desc')
+        var24= "python var that is hard coded"
+        print(var24)
+        print(name, key)
         return 'Hello, World! this is a post request'
     if request.method == 'PUT':
         return 'Hello, World! this is a put request'
     if request.method == 'GET':
         test_var = 'changed Hello, World! this is a get request, and this is a variable from my flask server!'
 
-        return render_template('test.html', jinja_var1=test_var, jinja_var2='this is another variable from server')
+        return render_template('form_test.html', jinja_var1=test_var, jinja_var2='this is another variable from server')
+
+
+
+@app.route('/add_data', methods=['GET', 'POST'])
+def hello_world_2():
+    if request.method == 'POST':
+        name = request.form.get('l_name')
+        key = request.form.get('l_desc')
+        add_user_data=User(username=name, passkey=key)
+        db.session.add(add_user_data)
+        db.session.commit()
+        return 'data was added to the db'
+    if request.method == 'GET':
+        return render_template('form_test.html')
+
+@app.route('/get_data_abcd', methods=['GET'])
+def get_data():
+    users_data = User.query.all()
+    print(users_data)
+    for user in users_data:
+        print(user.email)
+        print(user.password)
+        print(user.id)
+    # print(users.username)
+    # print(users.passkey)    
+    # print(users.userid)
+    # return 'data was fetched from the db'
+    return render_template('test.html', users=users_data)
+
+'''@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    if request.method == 'POST':
+        name = request.form.get('user')
+        key = request.form.get('pass')
+        print(name, key)
+        users_data = User.query.filter_by(username=name).first()
+        if users_data and users_data.passkey == key:
+            print(url_for('get_data'))
+            session["session_user"] = users_data.userid
+            return redirect(url_for('get_data'))
+        else:
+            return 'user is not present' #redirect(url_for('register'))'''
 
 if __name__ == '__main__':
     db.create_all()
+    # add_data()
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,11 +1,39 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.mutable import MutableList
+from sqlalchemy.orm import validates, relationship, backref
+from flask_security import UserMixin, RoleMixin, AsaList
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    userid = db.Column(db.Integer, autoincrement=True, primary_key=True) #user
-    username = db.Column(db.String, unique=True, nullable=False)#user's login id		
-    passkey = db.Column(db.String, nullable=False)				#user's password
+class RolesUsers(db.Model):
+    __tablename__ = 'roles_users'
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column('user_id', db.Integer(), db.ForeignKey('user.id'))
+    role_id = db.Column('role_id', db.Integer(), db.ForeignKey('role.id'))
+
+class Role(db.Model, RoleMixin):
+    __tablename__ = 'role'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+    permissions = db.Column(MutableList.as_mutable(AsaList()), nullable=True)
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
+    # username = db.Column(db.String(255), unique=True, nullable=True)
+    password = db.Column(db.String(255), nullable=False)
+    # last_login_at = db.Column(db.DateTime())
+    # current_login_at = db.Column(db.DateTime())
+    # last_login_ip = db.Column(db.String(100))
+    # current_login_ip = db.Column(db.String(100))
+    # login_count = db.Column(db.Integer)
+    active = db.Column(db.Boolean())
+    fs_uniquifier = db.Column(db.String(64), unique=True, nullable=False)
+    # confirmed_at = db.Column(db.DateTime())
+    roles = relationship('Role', secondary='roles_users',
+                         backref=backref('users', lazy='dynamic'))
 
 class list(db.Model):
 	list_id = db.Column(db.Integer, autoincrement=True, primary_key=True) #list
